df6a47f1e9ecc0a455c648d0143f143542574550
vadimver <vadimvereikin@gmail.com>
2018-07-19 13:24:22 +0300

    add delete office

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -3,8 +3,76 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Office;
+use App\User;
 
 class OfficeController extends Controller
 {
-    //
+    public function show($id)
+    {
+
+        $office = Office::select('offices.number','offices.created_at', 'regions.name as region_name', 'cities.name as city_name')
+                ->where('offices.id', $id)
+                ->join('regions', 'offices.region_id', '=', 'regions.id')
+                ->join('cities', 'offices.city_id', '=', 'cities.id')
+                ->get();
+        
+        if (!$office) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Office with id ' . $id . ' not found'
+            ], 400);
+        }
+           
+        return response()->json([
+            'success' => true,
+            'office' => $office,
+        ], 400);
+    }
+    
+    public function delete($id)
+    {
+        $role = auth()->user()->role;
+        
+        if ($role == 1) {
+            if (Office::where('id', '=', $id)->delete()) {
+                return response()->json([
+                    'success' => true
+                ]);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Office could not be deleted'
+                ], 500);
+            }
+        }
+    }
+    
+    public function create(Request $request)
+    {   
+        $role = auth()->user()->role;
+        
+        $request->validate([
+            'number' => ['required', 'unique:offices'],
+        ]);
+        
+        if ($role == 1) {
+            $office = new Office;
+
+            $office->region_id = $request->region_id;
+            $office->city_id = $request->city_id;
+            $office->number = $request->number;
+
+            $office->save();
+            
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'You don\'t have permission to perform this action.'
+            ], 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ Route::post('login', 'UserController@login')->name('login');
 Route::post('register', 'UserController@register');
 
 Route::post('packages/show_tracking', 'PackageController@show_tracking');
+
+Route::get('offices/show/{id}', 'OfficeController@show');
  
 Route::middleware('auth:api')->group(function () {
     Route::get('user_info', 'UserController@details');
@@ -25,5 +27,8 @@ Route::middleware('auth:api')->group(function () {
     
     Route::put('packages/status_update/{id}', 'PackageController@status_update');
     Route::get('packages/show/{id}', 'PackageController@show');
+    
+    Route::delete('offices/delete/{id}', 'OfficeController@delete');
+    Route::post('offices/create', 'OfficeController@create');
 });
 
